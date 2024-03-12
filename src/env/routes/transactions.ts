ec9d06fbf0ff0eb6085abafde26388236b4161dc
Otavio Goulart <otaviobgoulartt@gmail.com>
2024-03-11 22:31:31 -0300

    feat: select transations

--- src/env/routes/transactions.ts
+++ src/env/routes/transactions.ts
@@ -1,10 +1,29 @@
 import { FastifyInstance } from "fastify";
 import { knex } from "../../database";
-import { createTransactionsBodySchema } from "../../models";
+import { createTransactionIdSchema, createTransactionsBodySchema } from "../../models";
 import { randomUUID } from "crypto";
 
 
 export async function transactionsRoutes(app: FastifyInstance) {
+    app.get("/", async (req, res) => {
+
+        const transactions = await knex("transaction").select(); 
+
+        return { transactions };
+    });
+
+    app.get("/:id", async (req, res) => {
+
+        const { id } = createTransactionIdSchema.parse(req.params);
+
+        const transactions = await knex("transaction").where("id",id).first()
+
+        return { transactions };
+
+
+    })
+
+
     app.post("/", async (req, res) => {
         console.log("oi")
 
