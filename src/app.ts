d1d9459ca7709a1c27c13d6f2e19f668e52f898c
Otavio Goulart <otaviobgoulartt@gmail.com>
2024-03-19 21:31:08 -0300

    feat: first test using vitest

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { env } from "./env";
+import fastify from "fastify";
+import { knex } from "./database";
+import crypto from "node:crypto";
+import { transactionsRoutes } from "./routes/transactions";
+import fastifyCookie from "@fastify/cookie";
+
+export const app = fastify();
+
+app.register(fastifyCookie)
+app.register(transactionsRoutes, {
+    prefix: "transactions"
+});
